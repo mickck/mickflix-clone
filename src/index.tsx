8e0754e97f2895e230b62829c70a354ca0105ff6
Mickck <mikemwkim@gmail.com>
2022-12-06 11:22:02 +1100

    back

--- src/index.tsx
+++ src/index.tsx
@@ -77,16 +77,16 @@ const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  // <React.StrictMode>
-  <RecoilRoot>
-    <QueryClientProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </RecoilRoot>
+  <React.StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>{" "}
+  </React.StrictMode>
 );
 {
-  /* </React.StrictMode> */
 }
